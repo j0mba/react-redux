8d084417fa7e10d74f2ced0352ae378cef61ffe0
Lucas <sousa.sje@gmail.com>
2019-02-19 20:31:52 -0300

    React and webpack finished

--- exercicios_react/ex/index.jsx
+++ exercicios_react/ex/index.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ClassComponent from "./classComponent";
+import Field from "./field";
 
-ReactDOM.render(
-  <ClassComponent label="Contador" initialValue={10} />,
-  document.getElementById("app")
-);
+ReactDOM.render(<Field initialValue="Teste" />, document.getElementById("app"));
