b3174abbb69fc54d0b61cd47d6bf80e31ff716f5
Lucas Sousa Campos <sousa.sje@gmail.com>
2019-02-20 11:31:42 -0300

    postman

---
+++ todo-app/backend/src/api/todo/todo.js
@@ -0,0 +1,9 @@
+const restful = require("node-restful");
+const mongoose = restful.mongoose;
+const todoSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  done: { type: Boolean, required: true, default: false },
+  CreatedAt: { type: Date, default: Date.now }
+});
+
+module.exports = restful.model("Todo", todoSchema);
