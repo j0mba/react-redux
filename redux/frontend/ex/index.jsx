181190508e8cdba043e949caa7f46e7ce7ae9582
Lucas <sousa.sje@gmail.com>
2019-02-21 18:50:58 -0300

    First aplication complete

---
+++ redux/frontend/ex/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { combineReducers, createStore } from "redux";
+import { Provider } from "react-redux";
+import counterReducer from "./counterReducer";
+import Counter from "./counter";
+
+const reducers = combineReducers({
+  counter: counterReducer
+});
+ReactDOM.render(
+  <Provider store={createStore(reducers)}>
+    <Counter initialValue="Teste" />
+  </Provider>,
+  document.getElementById("app")
+);
